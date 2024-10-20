7120245189696e54248b19181988b320e33d7583
Almin <alminnoone2@gmail.com>
2024-10-20 17:38:46 +0200

    Created socket on logout listener

--- backend/server.js
+++ backend/server.js
@@ -56,14 +56,6 @@ io.on("connection", (socket) => {
         socket.nsp.to(uData._id).emit("get active users", activeUsers);
     })
 
-    // socket.on("join chat", (room) => {
-    //     socket.join(room);
-    //     console.log("User Joined Room: " + room);
-    // });
-
-    // socket.on("typing", (room) => socket.in(room).emit("typing"));
-    // socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
     socket.on("new message", (newMessageReceived) => {
         var chat = newMessageReceived.chat;
 
@@ -89,71 +81,20 @@ io.on("connection", (socket) => {
         activeUsers && activeUsers.forEach((active) => {
             socket.to(active).emit("get active users", activeUsers);
         });
-    })
+    });
 
-    socket.off("setup", () => {
-        console.log("USER DISCONNECTED");
+    socket.on("log out", (userData) => {
+        console.log("USER LOGGED OUT");
         socket.leave(userData._id);
+
+        const rooms = Array.from(socket.rooms);
+        activeUsers.forEach((active, index) => {
+            if(!rooms.includes(active)) {
+                activeUsers.splice(index+1);
+            }
+        });
+        activeUsers && activeUsers.forEach((active) => {
+            socket.to(active).emit("get active users", activeUsers);
+        });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// io.on("connection", (socket) => {
-//     console.log("Connected to socket.io")
-
-//     socket.on("setup", (userData) => {
-//         socket.join(userData._id);
-//         console.log(userData._id);
-//         socket.emit("connected");
-//     });
-
-//     socket.on("join chat", (room) => {
-//         socket.join(room);
-//         console.log(`User joined room ${room}`);
-//     });
-
-//     socket.on("new message", (newMessageReceived) => {
-
-//         var chat = newMessageReceived.chat;
-
-//         if(!chat.users) return console.log("chat.users not defined!");
-
-//         chat.users.forEach(user => {
-//             if(user._id == newMessageReceived.sender._id) return;
-
-//             socket.in(chat._id).emit("message received", newMessageReceived);
-//         });
-//     });
-// })
